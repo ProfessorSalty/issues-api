f5997b75f2dc4b6d6a5b43dd8b7de41b4ffd5e62
Greg Smith <gregory_smith@gregoftheweb.com>
2020-06-25 11:05:50 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IssueListComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
